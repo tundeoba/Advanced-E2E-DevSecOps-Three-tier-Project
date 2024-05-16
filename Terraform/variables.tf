b68d9cbc72a8bbfd5f4f9a0f5964ab8a82410e1b
tundeoba <119335552+tundeoba@users.noreply.github.com>
2024-05-15 23:19:30 -0500

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,9 +1,27 @@
-variable "vpc-name" {vpc-094227b9098ba0c22}
-variable "igw-name" {rtb-05ac382c190a8c4f7}
-variable "rt-name" {rtb-05ac382c190a8c4f7}
-variable "subnet-name" {subnet-0d0373c2f3404d311}
-variable "sg-name" {sg-02ccc3ce69dcd5d0a}
-variable "instance-name" {}
-variable "ami" {ami-0083d3f8b2a6c7a81}
-variable "key-name" {root-key}
-variable "iam-role" {}
+variable "vpc-name" {
+  default = "vpc-094227b9098ba0c22"
+}
+variable "igw-name" {
+  default = "rtb-05ac382c190a8c4f7"
+}
+variable "rt-name" {
+  default = "rtb-05ac382c190a8c4f7"
+}
+variable "subnet-name" {
+  default = "subnet-0d0373c2f3404d311"
+}
+variable "sg-name" { 
+  default = "sg-02ccc3ce69dcd5d0a"
+}
+variable "instance-name" {
+  default = "main-ec2"
+}
+variable "ami" { 
+  default = "ami-0083d3f8b2a6c7a81"
+}
+variable "key-name" {
+  default "root-key"
+}
+variable "iam-role" {
+  default "
+}
